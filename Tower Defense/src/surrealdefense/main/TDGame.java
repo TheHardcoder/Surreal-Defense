cb134be64037a992903329ae1c3950d59f710272
Ben <benni-vi@gmx.net>
2012-01-18 11:02:17 +0100

    Added new background

--- Tower Defense/src/surrealdefense/main/TDGame.java
+++ Tower Defense/src/surrealdefense/main/TDGame.java
@@ -3,15 +3,25 @@ package surrealdefense.main;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import surrealdefense.screens.AbstractScreen;
 import surrealdefense.screens.MainScreen;
+import surrealdefense.tools.ScreenTools;
 
 import com.golden.gamedev.Game;
 import com.golden.gamedev.GameLoader;
 
 public class TDGame extends Game {
 	public static TDGame game;
+	
+	{
+		distribute = false;
+	}
 
     /**
      * @param args the command line arguments
@@ -25,10 +35,19 @@ public class TDGame extends Game {
     }
     
     private AbstractScreen currentScreen = new MainScreen();
+    private BufferedImage background;
 
     @Override
     public void initResources() {
-        
+    	showCursor();
+    	this.bsGraphics.setWindowTitle("Surreal Defense");
+    	try {
+			this.bsGraphics.setWindowIcon(ImageIO.read(new File("resources/images/icon.png")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        background = ScreenTools.getDefaultGradient();
     }
 
     @Override
@@ -39,6 +58,7 @@ public class TDGame extends Game {
 
     @Override
     public void render(Graphics2D g) {
+    	g.drawImage(background, 0, 0, null);
         currentScreen.render(g);
     }
 }
--- Tower Defense/src/surrealdefense/tools/ScreenTools.java
+++ Tower Defense/src/surrealdefense/tools/ScreenTools.java
@@ -1,7 +1,9 @@
 package surrealdefense.tools;
 
+import java.awt.AlphaComposite;
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 import surrealdefense.main.Defaults;
@@ -11,16 +13,31 @@ import surrealdefense.main.Defaults;
  * @author D056866
  */
 public class ScreenTools {
+    private static BufferedImage defaultGradient;
     private static BufferedImage background;
+    
+    public static BufferedImage getDefaultGradient(){
+        if (defaultGradient != null)
+            return defaultGradient;
+        else {
+            defaultGradient = new BufferedImage(Defaults.windowWidth, Defaults.windowHeight, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g = defaultGradient.createGraphics();
+            g.setPaint(new GradientPaint(0,0, Defaults.backgroundColor1, defaultGradient.getWidth(),defaultGradient.getHeight(), Defaults.backgroundColor2));
+            g.fillRect(0, 0, defaultGradient.getWidth(), defaultGradient.getHeight());
+            return defaultGradient;
+        }
+    }
     
     public static BufferedImage getDefaultBackground(){
         if (background != null)
             return background;
         else {
-            background = new BufferedImage(Defaults.windowWidth, Defaults.windowHeight, BufferedImage.TYPE_INT_ARGB);
+        	background = new BufferedImage((int) (Defaults.windowWidth), (int) (Defaults.windowHeight), BufferedImage.TYPE_INT_ARGB);
             Graphics2D g = background.createGraphics();
-            g.setPaint(new GradientPaint(0,0, Defaults.backgroundColor1, background.getWidth(),background.getHeight(), Defaults.backgroundColor2));
-            g.fillRect(0, 0, background.getWidth(), background.getHeight());
+            g.setPaint(new GradientPaint(20,20, Defaults.backgroundColor2, background.getWidth()-40,background.getHeight()-40, Defaults.backgroundColor1));
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f));
+            g.fillRoundRect(20, 20, background.getWidth()-40, background.getHeight()-40,50,50);
             return background;
         }
     }
