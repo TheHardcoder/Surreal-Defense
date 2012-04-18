1ddddfaa96a7d155605a023f36060f5c8d45c7cf
Ben <benni-vi@gmx.net>
2012-04-19 00:30:38 +0200

    Added some comments.

--- Tower Defense/src/surrealdefense/screens/WorldMapScreen.java
+++ Tower Defense/src/surrealdefense/screens/WorldMapScreen.java
@@ -14,6 +14,7 @@ public class WorldMapScreen extends AbstractScreen {
 	protected WorldMap map;
 	protected LevelButton level1Button;
 	protected SaveGameDAO savegame;
+	private int diff = 0;
 
 	public WorldMapScreen(InputListener inputListener, SaveGameDAO save) {
 		super(inputListener);
@@ -41,10 +42,10 @@ public class WorldMapScreen extends AbstractScreen {
 	public void updateScreen(long elapsedTime) {
 		map.update(elapsedTime);
 		level1Button.setX(level1Button.getMapx() - scroll);
-		int diff = (int) (500.0*elapsedTime/1000); //500px pro Sekunde verschieben
-		if (inputListener.getMouseX() < width/5)
+		diff = (int) (500.0*elapsedTime/1000); //500px pro Sekunde verschieben
+		if (inputListener.getMouseX() < width/5) //Wenn Maus sich am linken Bildschirmrand befindet, nach links scrollen.
 			scroll = (int) ((scroll - diff < 0) ? 0 : (scroll - diff));
-		if (inputListener.getMouseX() > width*4/5)
+		if (inputListener.getMouseX() > width*4/5)// "               rechten "                            rechts "
 			scroll = (int) ((scroll + diff > width) ? width : (scroll +diff));
 		map.getBackground().setToCenter(scroll, 0, scroll+width, height);
 	}
